4ce949c2ae345114ae59e4f2791c84b883ac0ef7
Paulo H Mariano <51302275+paulodeab@users.noreply.github.com>
2024-12-18 21:46:04 -0300

    Add files via upload

---
+++ ProzAlmoxarifado/src/route/route.go
@@ -0,0 +1,50 @@
+package route
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type TemplateName string
+
+const (
+	IndexTemplate TemplateName = "index.html"
+	LoginTemplate TemplateName = "login.html"
+)
+
+var templates = map[TemplateName]*template.Template{
+	IndexTemplate: template.Must(template.ParseFiles("template/index.html")),
+	LoginTemplate: template.Must(template.ParseFiles("template/login.html")),
+}
+
+func GetTemplate(name TemplateName) *template.Template {
+	return templates[name]
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := GetTemplate(IndexTemplate)
+	tmpl.Execute(w, nil)
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+		username := r.FormValue("username");
+		password := r.FormValue("password");
+
+		fmt.Print(username);
+		fmt.Print(password);
+
+		if (username == "admin" && password == "1234") {
+			http.Redirect(w, r, "/index", http.StatusSeeOther)
+			return
+		}else{
+			fmt.Print("Falha ao logar");
+		}
+
+	}
+	tmpl := GetTemplate(LoginTemplate)
+	tmpl.Execute(w, nil)
+}
